0f0cf169934fddac0b9683b8c82de3a3d273834a
Cal <calebokyere1@gmail.com>
2023-04-24 17:02:22 +0000

    checking

--- main.h
+++ main.h
@@ -1,10 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * struct flags - A struct
- * @f: A char
- * @m: A function
- */
 int _putchar(char c);
 int print_string(char *s);
 int _printf(const char *format, ...);
